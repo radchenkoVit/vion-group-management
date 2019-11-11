43b94d932cb767e38c7c33f4f3996f26327e9ff0
v.radchenko <v.radchenko@yaypay.com>
2019-11-12 00:26:11 +0200

    route all requests to index page

--- backend/src/main/java/com/vion/backend/controller/mvc/MainController.java
+++ backend/src/main/java/com/vion/backend/controller/mvc/MainController.java
@@ -2,12 +2,18 @@ package com.vion.backend.controller.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping(value = {"/", "/index", "/register"})
+    @GetMapping(value = {"/", "/login", "/register"})
     public String main() {
         return "index";
+    }
+
+    @RequestMapping(value = "{_:^(?!index\\.html|api).$}")
+    public String redirectApi() {
+        return "forward:/";
     }
 }
